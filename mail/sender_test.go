cae5205d2709ac3e1b5514c89ddd452d51786a97
Youngjin Jo <moolairdev@gmail.com>
2024-10-16 21:35:27 -0700

    email set up in go

---
+++ mail/sender_test.go
@@ -0,0 +1,30 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/moolair/simplebank-golang-backend/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+	<h1>Hello world</h1>
+	<p>This is a test message from <a href="http://moolair.com">moolair</a></p>
+	`
+	to := []string{"moolairdev@gmail.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
