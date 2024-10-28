c84ca72224ff5be2fe088d6bfe23c2eb2bbcba64
Youngjin Jo <moolairdev@gmail.com>
2024-10-27 21:32:26 -0700

    Docker compose: port + volume mapping

--- api/user_test.go
+++ api/user_test.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
-	"github.com/lib/pq"
 	mockdb "github.com/moolair/simplebank-golang-backend/db/mock"
 	db "github.com/moolair/simplebank-golang-backend/db/sqlc"
 	"github.com/moolair/simplebank-golang-backend/util"
@@ -114,7 +113,7 @@ func TestCreateUserAPI(t *testing.T) {
 				store.EXPECT().
 					CreateUser(gomock.Any(), gomock.Any()).
 					Times(1).
-					Return(db.User{}, &pq.Error{Code: "23505"})
+					Return(db.User{}, db.ErrUniqueViolation)
 			},
 			checkResponse: func(recorder *httptest.ResponseRecorder) {
 				require.Equal(t, http.StatusForbidden, recorder.Code)
